74d2ace069f472cbfb1c7671675fbdef7dfd312b
Ergashev21 <t.ergashev2120@gmail.com>
2026-05-05 12:37:34 +0500

    Sinov

--- Program.cs
+++ Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Birnechta sonlar ustida amallar ");
+string misol = Console.ReadLine();
+int result = 0;
+char son = misol[0];
+char amal = misol[1];
+
+
+for (int i =1; i < misol.Length+1; i+2);
+{
+    for (int j = 0; j < misol.Length+1;j+2)
+    {
+        if (misol[i] == "+")
+        {
+            result += misol[j];
+        }
+        else if(misol[i] == "-")
+        {
+            result-= misol[j];
+        }
+          else if(misol[i] == "*")
+        {
+            result *= misol[j];
+        }
+          else if(misol[i] == "/")
+        {
+            result /= misol[j];
+        }
+        else
+        {
+            Console.WriteLine("Natija Chiqmadi ");
+        }
+        
+    }
+}
+
